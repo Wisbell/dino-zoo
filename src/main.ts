9949fb8dddacdad71782e18bcd0b6a5ef0df466c
West <coolwest@gmail.com>
2020-02-26 16:06:30 -0600

    Remove import

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory, HttpAdapterHost, AbstractHttpAdapter } from '@nestjs/core'
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { Express } from 'Express';
+// import { Express } from 'Express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
