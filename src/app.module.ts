618140b6329a3c50dedfd80e5c4967ad728844b0
West <coolwest@gmail.com>
2020-02-26 22:52:39 -0600

    Use yml files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,23 @@ import { PersonnelModule } from './personnel/personnel.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Animal } from './animals/animal.entity';
 import { AnimalsModule } from './animals/animal.module';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     AnimalsModule,
     PersonnelModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'test',
-      entities: [Animal],
-      synchronize: true,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: 'localhost',
+    //   port: 5432,
+    //   username: 'postgres',
+    //   password: 'postgres',
+    //   database: 'test',
+    //   entities: [Animal],
+    //   synchronize: true,
+    // }),
+    TypeOrmModule.forRoot(typeOrmConfig),
   ],
   controllers: [AppController]
 })
