2fbca5cb8336849cbb080fa19e064e790b5c0c9c
West <coolwest@gmail.com>
2020-02-27 20:47:30 -0600

    Remove unused folder and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { PersonnelModule } from './personnel/personnel.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Animal } from './animals/animal.entity';
 import { AnimalsModule } from './animals/animal.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TrainerModule } from './trainer/trainer.module';
@@ -11,7 +9,6 @@ import { KeeperModule } from './keeper/keeper.module';
 @Module({
   imports: [
     AnimalsModule,
-    PersonnelModule,
     TypeOrmModule.forRoot(typeOrmConfig),
     TrainerModule,
     KeeperModule,
