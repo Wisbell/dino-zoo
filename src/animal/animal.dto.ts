f59c7e6d11486af2175bab5c7ddbed8351664662
West <coolwest@gmail.com>
2020-03-02 15:08:44 -0600

    Add constructor to AnimalDto

--- src/animal/animal.dto.ts
+++ src/animal/animal.dto.ts
@@ -3,6 +3,24 @@ import { AnimalCategory } from "./animal-category.enum";
 import { IsEnum, IsString } from "class-validator";
 
 export class AnimalDto {
+  constructor(
+    name: string,
+    species: string,
+    gender: string,
+    age: string,
+    numberOfKills: string,
+    imageUrl: string,
+    category: string
+  ) {
+    this.name = name;
+    this.species = species;
+    this.gender = gender;
+    this.age = age;
+    this.numberOfKills = numberOfKills;
+    this.imageUrl = imageUrl;
+    this.setCategory(category);
+  }
+
   @IsString()
   name: string;
 
@@ -46,5 +64,16 @@ export class AnimalDto {
     animal.imageUrl = imageUrl;
     animal.category = category;
     return animal;
+  }
+
+  setCategory(category: string): void {
+    if (category === AnimalCategory.LARGE)
+      this.category = AnimalCategory.LARGE;
+
+    else if (category === AnimalCategory.MEDIUM)
+      this.category = AnimalCategory.MEDIUM;
+
+    else if (category === AnimalCategory.SMALL)
+      this.category = AnimalCategory.SMALL;
   }
 }
