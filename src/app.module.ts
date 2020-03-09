d80e84aa0098f9ec48bd41d49e2b0b539bb0eb6e
West <coolwest@gmail.com>
2020-03-09 16:32:59 -0500

    Update ORM config import so it works with migrations and typescript

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnimalModule } from './animal/animal.module';
-import { typeOrmConfig } from './config/typeorm.config';
+import * as typeOrmConfig from './config/typeorm.config';
 import { TrainerModule } from './trainer/trainer.module';
 import { KeeperModule } from './keeper/keeper.module';
 import { PersonnelModule } from './personnel/personnel.module';
