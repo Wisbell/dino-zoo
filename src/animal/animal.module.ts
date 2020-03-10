a4eee48c0214fe6e20785d9e726f79db14540045
West <coolwest@gmail.com>
2020-03-09 23:05:49 -0500

    Add TrainerService and AnimalService for use in module

--- src/animal/animal.module.ts
+++ src/animal/animal.module.ts
@@ -3,10 +3,18 @@ import { AnimalService } from './animal.service';
 import { AnimalController } from './animal.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnimalRepository } from './animal.repository';
+import { TrainerService } from '../trainer/trainer.service';
+import { TrainerRepository } from '../trainer/trainer.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AnimalRepository])],
-  providers: [AnimalService],
+  imports: [
+    TypeOrmModule.forFeature([AnimalRepository]),
+    TypeOrmModule.forFeature([TrainerRepository])
+  ],
+  providers: [
+    AnimalService,
+    TrainerService
+  ],
   controllers: [AnimalController]
 })
 export class AnimalModule {}
