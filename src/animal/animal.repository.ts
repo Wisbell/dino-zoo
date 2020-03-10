292edabbc1a315d3fe337b84d062da3be609cd7c
West <coolwest@gmail.com>
2020-03-09 23:07:10 -0500

    Add correct implementation of creating an animal with trainer.  Add todo for editing an animal with a trainer

--- src/animal/animal.repository.ts
+++ src/animal/animal.repository.ts
@@ -1,16 +1,17 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { Animal } from './animal.entity';
 import { AnimalDto } from './animal.dto';
+import { Trainer } from '../trainer/trainer.entity';
 
 @EntityRepository(Animal)
 export class AnimalRepository extends Repository<Animal> {
-  async createAnimal(createAnimalDto: AnimalDto) {
-    const animal = AnimalDto.toAnimal(createAnimalDto);
+  async createAnimal(createAnimalDto: AnimalDto, trainer: Trainer) {
+    const animal = AnimalDto.toAnimal(createAnimalDto, trainer);
     return animal.save();
   }
 
   async updateAnimal(existingAnimal: Animal, updatedAnimalDto: AnimalDto) {
-    const updatedAnimal = AnimalDto.toAnimal(updatedAnimalDto);
+    const updatedAnimal = AnimalDto.toAnimal(updatedAnimalDto, null); // TODO: FIX THIS
     existingAnimal.name = updatedAnimal.name;
     existingAnimal.species = updatedAnimal.species;
     existingAnimal.gender = updatedAnimal.gender;
