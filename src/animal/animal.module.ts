b672fd15d1bf679a4537f6fe6638f72a82622f7b
West <coolwest@gmail.com>
2020-03-13 13:55:18 -0500

    Add keeper import/provider

--- src/animal/animal.module.ts
+++ src/animal/animal.module.ts
@@ -5,15 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnimalRepository } from './animal.repository';
 import { TrainerService } from '../trainer/trainer.service';
 import { TrainerRepository } from '../trainer/trainer.repository';
+import { KeeperRepository } from '../keeper/keeper.repository';
+import { KeeperService } from '../keeper/keeper.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([AnimalRepository]),
-    TypeOrmModule.forFeature([TrainerRepository])
+    TypeOrmModule.forFeature([TrainerRepository]),
+    TypeOrmModule.forFeature([KeeperRepository])
   ],
   providers: [
     AnimalService,
-    TrainerService
+    TrainerService,
+    KeeperService
   ],
   controllers: [AnimalController]
 })
